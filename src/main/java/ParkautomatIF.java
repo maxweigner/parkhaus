80d0af380da347633a9d344fe0fc50a3fb7a00cc
hbrsRichi <rhartu2s@smail.inf.h-brs.de>
2023-04-13 23:27:23 +0200

    Realsierung der Ticket-Klasse

--- src/main/java/ParkautomatIF.java
+++ src/main/java/ParkautomatIF.java
@@ -1,4 +1,20 @@
 public interface ParkautomatIF {
 
+    /**
+     * erstellt ein neues Ticket für einfahrendes Fahrzeug
+     * @return: erstelltes Ticket
+     */
+    TicketIF erstellen();
+    /**
+     * verarbeitet Bezahlvorgang
+     * @param ticket: gültiges Ticket
+     * @return: true für bezahlt, false für offener Betrag
+     */
     boolean bezahlen(TicketIF ticket);
+
+    /**
+     * entwertet Ticket, indem Auto ausfährt und die Schranke sich öffnet
+     * @return true für entwertetes Ticket, false für Komplikationen bei der Ausfahrt
+     */
+    boolean entwerten();
 }
--- src/main/java/Ticket.java
+++ src/main/java/Ticket.java
@@ -1,2 +1,31 @@
-public class Ticket {
+public class Ticket implements TicketIF{
+    private int id;
+    private int preis;
+    private TimeIF start;
+    private TimeIF ende;
+
+    public Ticket(int id, int preis, TimeIF start){
+        this.id = id;
+        this.preis = preis;
+        this.start = start;
+    }
+    @Override
+    public int getID() {
+        return this.id;
+    }
+
+    @Override
+    public int getPreis() {
+        return this.preis;
+    }
+
+    @Override
+    public TimeIF getStart() {
+        return this.start;
+    }
+
+    @Override
+    public TimeIF getEnde() {
+        return this.ende;
+    }
 }
