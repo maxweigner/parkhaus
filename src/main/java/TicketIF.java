46c9b538117655848567d8dde57c508a5f76b7f8
hbrsRichi <rhartu2s@smail.inf.h-brs.de>
2023-04-13 23:15:52 +0200

    Anlegen TicketIF und der Testklasse, um Parkautomaten zu realsieren

--- src/main/java/TicketIF.java
+++ src/main/java/TicketIF.java
@@ -1,3 +1,25 @@
 public interface TicketIF {
+    /**
+     * eindeutiger Schlüssel
+     * @return: ID als int
+     */
+    int getID();
 
+    /**
+     * Preis pro Stunde
+     * @return: Stundenpreis als int
+     */
+    int getPreis();
+
+    /**
+     * Einfahrtszeitpunkt ins Parkhaus
+     * @return: Zeitstempel von TimeIF
+     */
+    TimeIF getStart();
+
+    /**
+     * Zahlungszeitpunkt am Automaten
+     * @return: Zeitstempel von TimeIF
+     */
+    TimeIF getEnde();
 }
