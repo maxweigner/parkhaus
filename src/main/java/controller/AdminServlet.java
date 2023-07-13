439d85c50aa633bc4910f383ce3f8013d07cb99b
Maximilian Wagner <maximilian.wagner@smail.inf.h-brs.de>
2023-07-13 14:52:18 +0200

    Visual refactoring AdminServlet

--- src/main/java/controller/AdminServlet.java
+++ src/main/java/controller/AdminServlet.java
@@ -39,27 +39,35 @@ public class AdminServlet extends HttpServlet {
             addParams(req); // falls die oeffnungszeiten sich nicht ändern, müssen die alten mitgegeben werden
             req.getRequestDispatcher("/checkInMonatsticket").forward(req, res);
             return;
-        } else if (aktion.equals("reset")) { // falls reset geklickt wurde
+        }
+
+        // Parkhaus zurücksetzen
+        if (aktion.equals("reset")) { // falls reset geklickt wurde
             ParkhausIF ph = new Parkhaus(); // erzeugt neues Parkhaus
             getServletContext().setAttribute("parkhaus", ph); // setzt Parkhaus in der DB
             addParams(req); // setze Einnahmen von null auf 0
             ParkhausServlet.doOnEveryRequest(req, ph); // Endroutine
             req.getRequestDispatcher("admin.jsp").forward(req, res);
+            return;
         }
 
-        // ticket id holen
+
         String ticketNr = req.getParameter("ticket");
         String ticketPreis = req.getParameter("preis");
         String globalPreis = req.getParameter("preisGlobal");
         String oeffnungszeit = req.getParameter("oeffnungszeit");
         String schliesszeit = req.getParameter("schliesszeit");
 
-        // wenn die eingabe unvollständig ist
+        // Setzen vom Globalpreis
         if(globalPreis != null) {
             getServletContext().setAttribute("globalPreis", Integer.parseInt(globalPreis));
-        } else if (ticketNr != null && ticketPreis != null) {
+        }
+        // Setzen von einzelnem Ticketpreis
+        else if (ticketNr != null && ticketPreis != null) {
             parkhaus.getTicket(Integer.parseInt(ticketNr)).setPreis(Integer.parseInt(ticketPreis));
-        } else if (oeffnungszeit != null && schliesszeit != null) {
+        }
+        // Setzen der Öffnungszeit
+        else if (oeffnungszeit != null && schliesszeit != null) {
             getServletContext().setAttribute("oeffnungszeit", LocalTime.parse(oeffnungszeit, DateTimeFormatter.ISO_LOCAL_TIME).minusMinutes(1));
             getServletContext().setAttribute("schliesszeit", LocalTime.parse(schliesszeit, DateTimeFormatter.ISO_LOCAL_TIME).plusMinutes(1));
         }
